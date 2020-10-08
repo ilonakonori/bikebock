d5911c0f4b55b522894e7f07e33d9bf2d630c3cc
iva mudronja <iva.pilizota@gmail.com>
2020-10-08 12:59:50 +0200

    cloudinary config, user photo, .env file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :photo
   # validations
   validates :name, presence: true, uniqueness: true, length: { in: 2..20 }, format: { with: /\A[a-zA-Z]+\z/ }
   validates :about_me, presence: true, length: { in: 20..600 }
+  validates :photo, presence: true
   # validates :date_of_birth, presence: true # wip
 end
