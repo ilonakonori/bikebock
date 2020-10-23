834c57866a01683f858cf6799d76fea3eb4d37d5
iva mudronja <iva.pilizota@gmail.com>
2020-10-24 01:20:25 +0200

    user date_of_birth min & max year, validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,16 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { in: 2..20 }, format: { with: /\A[a-zA-Z]+\z/ }
   validates :about_me, presence: true, length: { in: 20..600 }
   validates :photo, presence: true
-  # validates :date_of_birth, presence: true # wip
+  validates :date_of_birth, presence: true
+
+  # validate date => custom method
+  validate :date_of_birth_validation
+
+  def date_of_birth_validation # mmaybe? min & max => JS datepicker or smtng?
+    if date_of_birth.present? && date_of_birth > (Date.today - 5843.87518)
+      errors.add(:date_of_birth, "You have to be at least 16 years old")
+    end
+  end
 
   def age
     ((Date.today - date_of_birth).to_i / 365.242199).floor
