c61ced09c16eb34d3e2c99ad913cbe46d7ab9d05
Ilona Kozenkovaite <kozenkovaite@gmail.com>
2021-02-04 15:04:19 +0100

    email validation add

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  before_save { self.email = email.downcase }
 
   has_one_attached :profile_photo
   has_one_attached :bike_photo
