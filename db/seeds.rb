f5b802974ba40c0b83780b2e2f7b66ce4944b2be
Ilona Kozenkovaite <kozenkovaite@gmail.com>
2021-02-06 22:22:09 +0100

    inerests seeds uppercase

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ Ride.destroy_all
     email: "luis7villasmil1+@gmail.com",
     password: "123456",
     about_me: "To me it doesn’t matter whether it’s raining or the sun is shining or whatever: as long as I’m riding a bike, I know I’m the luckiest guy in the world. Cycling is my passion.",
-    interests: "Learning languages. Playing piano. Snow boarding. Climbing. Hiking. Cooking."
+    interests: "LANGUAGES SNOWBOARDING CLIMBING HIKING COOKING"
     )
   puts "User not saved!"
     image = URI.open("https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=935&q=80")
@@ -70,7 +70,7 @@ end
     email: "fiona3schlender2+@gmail.com",
     password: "123456",
     about_me: "I am a deltiologist or simply, a person who collects postcard and I absolutely love cooking. I can merrily state that my kitchen is my playground in every sense!",
-    interests: "Science. Politics. Literature. Concerts. Theatres. Exhibitions."
+    interests: "SCIENE POLITICS LITERATURE CONCERTS THEATRES EXHIBITIONS"
     )
   puts "User not saved!"
     image = URI.open("https://images.unsplash.com/photo-1600207438283-a5de6d9df13e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80")
@@ -120,7 +120,7 @@ end
     email: "eden9ooms8+@gmail.com",
     password: "123456",
     about_me: "I love learning new languages. I am a fluent speaker of 4 languages and I want to learn a few more. At present, I am learning Spanish and German. I have a tutor to learn German. And I enjoy cycling and swimming.",
-    interests: "Literature. Travel. Music. Culture. Arts. Skateboring."
+    interests: "LITERATURE TRAVEL MUSIC CULTURE ARTS SKATEBORDING"
     )
   puts "User not saved!"
     image = URI.open("https://images.unsplash.com/photo-1508511267-5a04ee04ca95?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80")
@@ -170,7 +170,7 @@ end
     email: "panitan4punpuang0+@gmail.com",
     password: "123456",
     about_me: "I take pleasure in listening to music. I listen to music every morning before I leave for work and when I return home every night. I also take pleasure in reading and travelling to new places.",
-    interests: "Philosophy. Science. Startups. Tree climbing. Board games. Yoga."
+    interests: "PHILOSOPHY SCIENCE STARTUPS YOGA TRAVEL DJ"
     )
   puts "User not saved!"
     image = URI.open("https://images.unsplash.com/photo-1600481176431-47ad2ab2745d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80")
@@ -221,7 +221,7 @@ end
     email: "ronant6kruithof3+@gmail.com",
     password: "123456",
     about_me: "I am a bird-watching enthusiast. I love the outdoors and enjoy going for hikes during weekends. Also, I often spend time reading nature books and also keep digging information on the internet about birds in this region.",
-    interests: "Art. Architecture. Design. Music. Media. Tech."
+    interests: "ART ARCHITECTURE DESIGN MUSIC MEDIA TECH"
     )
   puts "User not saved!"
     image = URI.open("https://images.unsplash.com/photo-1570632267781-46f97c2a4f76?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80")
@@ -272,7 +272,7 @@ end
     email: "martina5guido9+@gmail.com",
     password: "123456",
     about_me: "I enjoyed travelling the most. Yes, I am a wandering soul. Visiting new places, traveling new roads, trying new cuisines and learning about new cultures excites me! My dream is to cover all the countries, both small and big, before I turn 50!",
-    interests: "Sustainability. Politics. Economy. Clubing. Board games. Ice dipping."
+    interests: "SUSTAINABILITY POLITICS ECONOMY CLUBING ICE-DIPPING"
     )
   puts "User not saved!"
     image = URI.open("https://images.unsplash.com/photo-1588453383063-37ea0b78f30f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80")
@@ -322,7 +322,7 @@ end
     email: "jc1gellidon2+@gmail.com",
     password: "123456",
     about_me: "I love to write in my free time. I have tons of notebooks filled with plots, character sketches, and obviously some random doodles. I take delight in writing short stories about the experiences I encounter every day.",
-    interests: "Traveling. Cooking. Languages. Fitness. Meditation. Yoga."
+    interests: "TRAVELING COOKING LANGUAGES FITNESS MEDITATION YOGA"
     )
   puts "User not saved!"
     image = URI.open("https://images.unsplash.com/photo-1601062138836-c90756d8dce9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80")
@@ -372,7 +372,7 @@ end
     email: "joy0aragon6+@gmail.com",
     password: "123456",
     about_me: "I am fond of dancing, it is both my hobby and passion. Sometimes even during the day, I play music on my phone and dance my heart out, even if its for just 5 minutes! Dance helps me relax as it invigorates my soul. I am a trained Bharatnatyam dancer.",
-    interests: "Coffee. Travel. Vintage. Nature. Chess. Windsurfing."
+    interests: "COFEE TRAVEL VINTAGE NATURE CHESS WINDSURFING"
     )
   puts "User not saved!"
     image = URI.open("https://images.unsplash.com/photo-1592755219588-d4ff92a0d4de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80")
