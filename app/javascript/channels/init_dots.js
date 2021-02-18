65158d146ff2b0c93e9c14a13ae7b04b95e7e64b
iva mudronja <iva.pilizota@gmail.com>
2021-02-18 10:53:36 +0100

    deleted changeColor from .html, added init_dots.js

---
+++ app/javascript/channels/init_dots.js
@@ -0,0 +1,29 @@
+const initDots = () => {
+  function changeColor(class_name, i) {
+    const dots = document.getElementsByClassName(class_name);
+    while (i > 0) {
+      dots[i-1].style.backgroundColor = '#00917C';
+      i -= 1;
+    }
+  }
+
+  const diff = document.getElementById('noD'); //.dataset.diffId;
+  let difficulty = 0;
+  if (diff) {
+    difficulty += diff.dataset.diffId;
+    console.log(difficulty);
+  }
+
+  const people = document.getElementById('noP');
+  let numberOfPeople = 0;
+  if (people) {
+    numberOfPeople += people.dataset.peopleId;
+    console.log(numberOfPeople);
+  }
+
+
+  changeColor("dot d", difficulty);
+  changeColor("dot n", numberOfPeople);
+}
+
+export { initDots };
