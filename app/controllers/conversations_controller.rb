9ea9de6ef5b407dea84e59cb44eb861810f72ff9
iva mudronja <iva.pilizota@gmail.com>
2021-02-20 22:22:15 +0100

    added messages, added cable

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,10 +1,11 @@
 class ConversationsController < ApplicationController
-  def index # wip
+  def index
     @conversations = policy_scope(Conversation).where(recipient_id: current_user) + policy_scope(Conversation).where(sender_id: current_user)
   end
 
   def show
     @conversation = Conversation.find(params[:id])
+    @message = Message.new
     authorize @conversation
   end
 
@@ -14,6 +15,12 @@ class ConversationsController < ApplicationController
     @request.update(accepted: true, friend: true)
     authorize @conversation
     if @conversation.save
+      c = Conversation.find(@conversation.id)
+      message = Message.create!(conversation_id: c.id, sender_id: c.sender_id, recipient_id: c.recipient_id, content: @request.first_message)
+
+      #ConversationChannel.broadcast_to(@conversation, render_to_string(partial: "message", locals: { message: @message }))
+      #redirect_to conversation_path(@conversation) #, anchor: "message-#{@message.id}")
+
       redirect_to conversation_path(@conversation), notice: "Request accepted, start conversation :)"
     else
       render 'requests/show'
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,6 +3,6 @@ class Conversation < ApplicationRecord
   has_many :messages
 
   def opposed_user(user)
-    user.id == recipient_id ? User.find(sender_id).name : User.find(recipient_id).name
+    user.id == recipient_id ? User.find(sender_id) : User.find(recipient_id)
   end
 end
