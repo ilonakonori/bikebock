134379938cec7cd51735376d4f015be4b4a78a5d
iva mudronja <iva.pilizota@gmail.com>
2021-03-10 21:14:00 +0100

    wip: added attachment to msg

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,12 @@
 class Message < ApplicationRecord
   belongs_to :conversation
-  validates :content, presence: true
+  # validates :content, presence: true
+  has_one_attached :attachment
+
+  validates :content, presence: true, unless: ->(message){message.attachment.present?}
+  validates :attachment, presence: true, unless: ->(message){message.content.present?}
+
+  def download_link
+    #Cloudinary::Utils.private_download_url self.cloudinary_id, self.format, attachment: true
+  end
 end
