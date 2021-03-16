349da43eb7ff90ef378f2c00de0c22615f00643c
iva mudronja <iva.pilizota@gmail.com>
2021-03-16 14:06:05 +0100

    deleted unnecessary, remove style to scss file

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,8 +4,4 @@ class Message < ApplicationRecord
 
   validates :content, presence: true, unless: ->(message){message.attachment.present?}
   validates :attachment, presence: true, unless: ->(message){message.content.present?}
-
-  #def download_link
-    #Cloudinary::Utils.private_download_url self.cloudinary_id, self.format, attachment: true
-  #end
 end
