e33fa2ce14a39b7737be5864908d864e14829cb1
iva mudronja <iva.pilizota@gmail.com>
2021-04-08 13:25:52 +0200

    close request form when clicked outside

--- app/javascript/components/init_request_form.js
+++ app/javascript/components/init_request_form.js
@@ -18,6 +18,20 @@ const initRequestForm = () => {
       requestBtn.scrollIntoView(false);
     })
   }
+
+  // close requestBox when clicked outside
+  if(requestBox) {
+    document.addEventListener('click', function(event) {
+      let el = event.target;
+      const ids = [`myForm`, `f-box`, `new_request`, `request-btn`, `select2-request_ride_date-container`];
+      if(ids.includes(el.id) || el.tagName === `B`) {
+        // do nothing
+      } else {
+        requestBox.style.display = 'none';
+        requestBtn.style.display = 'inherit';
+      }
+    })
+  }
 }
 
 export { initRequestForm };
