50d8062b7736e0164f5ccc857b4e0ec15cf19c97
iva mudronja <iva.pilizota@gmail.com>
2021-04-10 00:13:08 +0200

    removed js from html

---
+++ app/javascript/components/init_anchor_scroll.js
@@ -0,0 +1,19 @@
+const initAnchorScroll = () => {
+
+  if(window.location.hash) {
+    const el = document.querySelector(window.location.hash);
+    if(el){
+      el.scrollIntoView(false);
+      document.getElementById("messages").scrollTop += 24;
+    }
+  } else {
+    const showPage = document.getElementById("c-show");
+    const el = document.getElementById("messages");
+    if(showPage && el){
+      el.lastElementChild.scrollIntoView(false);
+      //el.scrollTop += 24;
+    }
+  }
+}
+
+export { initAnchorScroll };
