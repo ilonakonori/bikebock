a064a19179d1818c6a7aeb9b70289b8017b52069
iva mudronja <iva.pilizota@gmail.com>
2021-04-09 14:44:15 +0200

    added lockbox

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :conversation
   has_one_attached :attachment
+  encrypts :content, type: :text
 
   validates :content, presence: true, unless: ->(message){message.attachment.present?}
   validates :attachment, presence: true, unless: ->(message){message.content.present?}
