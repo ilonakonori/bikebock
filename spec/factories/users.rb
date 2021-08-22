2a45221cec5c3926619519a3301ee52a2a338e11
iva mudronja <iva.pilizota@gmail.com>
2021-08-22 19:16:38 +0200

    shoulda-matchers => user: basic tests of relations and some validations

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:name} { |n| "Mande_#{n}" }
+    sequence(:email) { |n| "mande_#{n}@mande.com" }
+    password { '12345678' }
+    ##
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # let(:user) { FactoryBot.create(:user) }
+  # relations
+  it { is_expected.to have_many :rides }
+  it { is_expected.to have_many :notifications }
+  it { is_expected.to have_many :requests }
+  it { is_expected.to have_many :bookings }
+  it { is_expected.to have_many :blockings }
+  it { is_expected.to have_one :tracking }
+  it { is_expected.to have_one :account }
+  # attached files
+  it { is_expected.to have_one_attached :profile_photo }
+  it { is_expected.to have_one_attached :bike_photo }
+  # validations
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :email }
+  it { is_expected.to validate_presence_of :password }
+  it { is_expected.to validate_presence_of :interests }
+  it { is_expected.to validate_presence_of :profile_photo }
+  # it { is_expected.to validate_uniqueness_of :name }
+  # it { is_expected.to validate_uniqueness_of :email }
+  it { is_expected.to validate_length_of(:name).is_at_least(2).on(:create) }
+  it { is_expected.to validate_length_of(:name).is_at_most(20).on(:create) }
+  it { is_expected.to validate_length_of(:password).is_at_least(8).on(:create) }
+  it { is_expected.to validate_length_of(:password).is_at_most(128).on(:create) }
+  it { is_expected.to validate_length_of(:interests).is_at_least(10).on(:create) }
+  it { is_expected.to validate_length_of(:interests).is_at_most(200).on(:create) }
+  # it { is_expected.to validate_format_of(:name).with(/[a-zA-Z0-9_]/) }
+end
